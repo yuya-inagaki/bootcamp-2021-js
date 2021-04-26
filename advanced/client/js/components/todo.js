144351957f81d2353886ae08b0d0ffe161ca60af
Yuya inagaki <yuya.ina56@gmail.com>
2021-04-26 17:10:44 +0900

    feat todo check update function

--- advanced/client/js/components/todo.js
+++ advanced/client/js/components/todo.js
@@ -4,12 +4,35 @@ class Todo {
     this.id = id
     this.name = name
     this.done = done
+    this.element = document.createElement("li");
+    this.element.className = "todo-item";
+  }
+
+  mount() {
+    const api = "http://localhost:3000/todo"
+    const checkbox = this.element.querySelector('.todo-toggle')
+    checkbox.addEventListener('click', () => {
+      fetch(api + '/' + this.id, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: this.name,
+          done: !this.done
+        }),
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Update:', data)
+        this.done = !this.done
+      })
+
+    })
   }
 
   render() {
-    const next = document.createElement("li");
-    next.className = "todo-item";
-    next.innerHTML = `
+    this.element.innerHTML = `
       <label class="todo-toggle__container">
         <input
           data-todo-id="${this.id}"
@@ -23,7 +46,8 @@ class Todo {
       <div class="todo-name">${this.name}</div>
       <div data-todo-id="${this.id}" class="todo-remove-button">x</div>
     `;
-    this.parent.appendChild(next);
+    this.parent.appendChild(this.element)
+    this.mount()
   }
 }
 
